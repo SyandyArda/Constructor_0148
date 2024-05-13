1f29f68cb8a1c68c529dee8eeb6fd31c8b15fb5d
SyandyArda <syandi.2901@gmail.com>
2024-05-13 11:08:38 +0700

    commit delapan

--- memberstatik1/memberstatik1.cpp
+++ memberstatik1/memberstatik1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class mahasiswa
 {
 private:
-	static int nim;
+	long long static int nim;
 public:
 	int id;
 	string nama;
@@ -23,7 +23,7 @@ public:
 	}
 };
 
-int mahasiswa::nim = 20230140148;
+long long int mahasiswa::nim = 20230140148;
 
 void mahasiswa::setID()
 {
@@ -33,5 +33,23 @@ void mahasiswa::setID()
 void mahasiswa::printAll()
 {
 	cout << "ID   = " << id << endl;
-	cout << ""
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main()
+{
+	mahasiswa mhs1("Arda");
+	mahasiswa mhs2("Dhito");
+	mahasiswa::setNim(9); // mengakses nim melalui static member function "setNim"
+	mahasiswa mhs3("Jihad");
+	mahasiswa mhs4("Galang");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl; //  mengakses
+
 }
