235e979552b0c6c14caaafe1463df9aebf05a3bc
SyandyArda <syandi.2901@gmail.com>
2024-05-13 11:01:19 +0700

    commit tujuh

--- memberstatik1/memberstatik1.cpp
+++ memberstatik1/memberstatik1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 class mahasiswa
 {
 private:
@@ -15,5 +16,22 @@ public:
 	static void setNim(int pNim) { nim = pNim; }
 	static int getNim() { return nim; }
 
-	mahasiswa(string pnama) : nama(pnama) { setID(); }
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setID();
+	}
 };
+
+int mahasiswa::nim = 20230140148;
+
+void mahasiswa::setID()
+{
+	id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+	cout << "ID   = " << id << endl;
+	cout << ""
+}
